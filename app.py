3e3bda9dd1449e96c3f06cb807a76b8167b40661
Amarendra Kumar Saroj <amarendra@nrb.org.np>
2025-05-06 16:57:32 +0545

    added login route

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, request, abort
+from flask import Flask, request, abort, jsonify
 from flask_bcrypt import Bcrypt
 from config import ApplicationConfig
 from models import db, User
@@ -7,24 +7,53 @@ from models import db, User
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 
-
+bcrypt = Bcrypt(app)
 db.init_app(app)
 
 with app.app_context():    
     db.create_all()
     
-@app.route("/register", method=["POST"])
+@app.route("/register", methods=["POST"])
 def register_user():
     email = request.json["email"]
     password = request.json["password"]
     
     user_exists = User.query.filter_by(email=email).first() is not None
     if user_exists:
-        abort(409)
+        return jsonify({"error":"user email already registered "})
         
-    new_user = User(email=email, password=password)
+    hashed_password = bcrypt.generate_password_hash(password)
+    new_user = User(email=email, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
 
+    return jsonify({
+        "id": new_user.id,
+        "email": new_user.email
+    })
+
+@app.route("/login", methods=["POST"])
+def login_user():
+    email = request.json["email"]
+    password = request.json["password"]
     
+    user = User.query.filter_by(email=email).first()
+    
+    if user is None:
+        return jsonify({
+            "error":"Unauthorized"
+        }), 401
+        
+    if bcrypt.check_password_hash(user.password, password):
+        return jsonify({
+            "error":"Unauthorized"
+        }), 401
+        
+    
+    return jsonify({
+        "id": user.id,
+        "email":  user.email
+    })
     
     
     
